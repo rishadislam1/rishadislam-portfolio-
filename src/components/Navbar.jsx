93e628973ed2df41f1dc8a867e0e0a6d96b0a969
Md Rishad Islam <rishad.islam158@gmail.com>
2026-06-13 11:44:47 +0600

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import {navIcons, navLinks} from "#constants/index.js";
+import dayjs from "dayjs";
+
+const Navbar = () => {
+
+    return (
+        <nav>
+            <div>
+                <img src={'/images/rishadislamSign.png'} alt={'logo'} className={`h-[40px]`}/>
+                <p className={`font-bold`}>Risahd's Portfolio</p>
+
+                <ul>
+                    {navLinks?.map(({id,name})=><li key={id}>
+                        <p>{name}</p>
+                    </li>)}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map(({id,img})=><li>
+                        <img src={img} className={"icon-hover"} alt={`icon-${id}`}/>
+                    </li>)}
+                </ul>
+
+                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
