934c9ea995862547da8f605fffec283df9cc9877
Md Rishad Islam <rishad.islam158@gmail.com>
2026-06-13 21:42:38 +0600

    implement welcome section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Navbar from "#components/Navbar.jsx";
+import Welcome from "#components/Welcome.jsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <main>
       <Navbar/>
+        <Welcome/>
     </main>
   )
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,88 @@
+import {useRef} from "react";
+import gsap from "gsap";
+import {useGSAP} from "@gsap/react";
+
+
+const FONT_WEIGHTS = {
+    subtitle: {min: 100, max: 400, default: 100},
+    title: {min: 400, max: 900, default: 400},
+}
+const renderText  = (text, classNem, baseWeight=400)=>{
+    return [... text].map((char,i)=>
+        <span
+            key={i}
+            className={classNem}
+            style={{
+                fontVariationSettings: `'wght' ${baseWeight}`,
+            }}
+        >
+            {char === " "?"\u00A0":char}
+        </span>
+    )
+}
+
+const setupTextHover = (container,type)=>{
+    if(!container) return;
+
+    const letters = container.querySelectorAll("span");
+    const {min,max,default:base} = FONT_WEIGHTS[type];
+    const animateLetters = (letter,weight,duration=0.25)=>{
+        return gsap.to(letter, {
+            duration,
+            ease: "power2.out",
+            fontVariationSettings: `'wght' ${weight}`,
+        });
+    };
+
+    const handleMouseMove = (e)=>{
+        const {left} = container.getBoundingClientRect();
+        const mouseX = e.clientX - left;
+
+        letters.forEach(letter=>{
+            const{left:l,width:w} = letter.getBoundingClientRect();
+            const distance = Math.abs(mouseX - (l-left + w/2));
+            const intensity = Math.exp(-(distance**2)/2000);
+            animateLetters(letter,min+(max-min)*intensity);
+        })
+    };
+
+    const handleMouseLeave = ()=>{
+        letters.forEach(letter=>animateLetters(letter,base,0.3));
+    }
+
+    return ()=>{
+        container.addEventListener("mousemove",handleMouseMove);
+        container.removeEventListener("mousemove",handleMouseLeave);
+    }
+
+}
+
+const Welcome = () => {
+    const titleRef = useRef(null);
+    const subtitleRef = useRef(null);
+
+    useGSAP(()=>{
+       const titleCleanup = setupTextHover(titleRef.current,"title");
+        const subTitelCleanup = setupTextHover(subtitleRef.current,"subtitle");
+
+        return () =>{
+            subTitelCleanup();
+            titleCleanup();
+        }
+    },[])
+
+    return (
+        <section id={`welcome`}>
+            <p ref={subtitleRef}>{renderText("Hey, I am Rishad ! Welcome to my ","text-3xl font-georama",100)}</p>
+            <h1 ref={titleRef} className={`mt-7`}>
+                {renderText("Portfolio","text-9xl font-georama",400)}
+            </h1>
+
+            <div className={`small-screen`}>
+                <p>This Portfolio is designed for desktop/tablet screens only.</p>
+            </div>
+        </section>
+    );
+};
+
+export default Welcome;
